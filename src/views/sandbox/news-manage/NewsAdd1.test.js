08c01362ff7f5822d7a63e2917f5d3a23daf2763
fhx <15079534562@163.com>
2025-02-23 22:28:24 +0800

    管理系统-jest

--- src/views/sandbox/news-manage/NewsAdd1.test.js
+++ src/views/sandbox/news-manage/NewsAdd1.test.js
@@ -1,5 +1,5 @@
 import React from "react";
-import NewsAdd from "./sandbox/news-manage/NewsAdd";
+import NewsAdd from "./NewsAdd";
 import userEvent from "@testing-library/user-event";
 import {
   act,
