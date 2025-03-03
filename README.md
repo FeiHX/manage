0920da5b8840043691683c183b033b89ea245869
fhx <15079534562@163.com>
2025-03-03 23:22:28 +0800

    manage-jest

--- README.md
+++ README.md
@@ -2,18 +2,40 @@
 客户端目录
 
 ```
-
+│  
 │  babel.config.js
+│  jest.config.js
+│  package-lock.json
 │  package.json
+│  README.md
 │  
 ├─config
-│      webpack.config.js
+│  │  webpack.config.js
+│  │  
+│  └─jest
+│          cssTransform.js
+│          fileTransform.js
+│          
+├─public
+│      favicon.ico
+│      index.html
+│      logo192.png
+│      logo512.png
+│      manifest.json
+│      mockServiceWorker.js
+│      robots.txt
 │      
+├─server
+│          
 └─src
+    │  .babelrc
+    │  App.css
     │  App.js
+    │  babel.config.js
     │  index.css
     │  index.js
     │  logo.svg
+    │  setupTests.js
     │  
     ├─components
     │  ├─news-manage
@@ -25,92 +47,128 @@
     │  │      
     │  ├─sandbox
     │  │      index.css
-    │  │      NewsRouter.js
     │  │      SideMenu.js
     │  │      TopHeader.js
     │  │      withRoute.js
     │  │      
-    │  └─user-manage
-    │          UserForm.js
+    │  ├─user-manage
+    │  │      UserForm.js
+    │  │      
+    │  └─__test__
+    │          NewsEditor.test.js
+    │          NewsPublish.test.js
+    │          SideMenu.test.js
+    │          TopHeader.test.js
     │          
     ├─image
     │      JiqGstEfoWAOHiTxclqi.png
     │      QasfAllzWOlzRzlqooai.jpg
+    │      
+    ├─mocks
+    │      handlers.js
+    │      server.js
     │      
     ├─redux
     │  │  store.js
     │  │  
     │  └─reducers
     │          CategoriesReducer.js
+    │          ChatMessageReducer.js
     │          CollapsedReducer.js
     │          CurrentUserReducer.js
+    │          NoticeListReducer.js
     │          RightListReducer.js
     │          RoleListReducer.js
     │          
-    │          
     ├─router
-    │      DetailRoute.js
-    │      IndexRouter.js
-    │      LoginRoute.js
-    │      NewsRoute.js
-    │      Redirect.js
+    │  │  DetailRoute.js
+    │  │  IndexRouter.js
+    │  │  LoginRoute.js
+    │  │  NewsRoute.js
+    │  │  Redirect.js
+    │  │  
+    │  └─__test__
+    │          IndexRouter.test.js
+    │          
+    ├─test
+    │      mockStore.js
     │      
     ├─utils
     │      data.js
-    │      echarts.js 
+    │      echarts.js
     │      myAxios.js
     │      
-    └─views
-        ├─login
-        │      login.css
-        │      Login.js
-        │      LoginForm.js
-        │      OtherLogin.js   
-        │      OtherLoginForm.js
-        │      
-        ├─news
-        │      Detail.css
-        │      Detail.js
-        │      News.css
-        │      News.js
-        │      
-        └─sandbox
-            │  NewsSandBox.css
-            │  NewsSandBox.js
-            │  
-            ├─audit-manage
-            │      Audit.js
-            │      AuditList.js
-            │      
-            ├─home
-            │      home.css
-            │      Home.js
-            │      
-            ├─news-manage
-            │      News.css
-            │      NewsAdd.css
-            │      NewsAdd.js
-            │      NewsCategory.js
-            │      NewsDraft.js
-            │      NewsPreview.css
-            │      NewsPreview.js
-            │      NewsUpdate.js
-            │      
-            ├─nopermission
-            │      Nopermission.css
-            │      NoPermission.js
-            │      
-            ├─publish-manage
-            │      Publish.js
-            │      Sunset.js
-            │      Unpublish.js
-            │      
-            ├─right-manage
-            │      RightList.js
-            │      RoleList.js
-            │      
-            └─use-manage
-                    UserList.js
+    ├─views
+    │  ├─login
+    │  │      login.css
+    │  │      Login.js
+    │  │      LoginForm.js
+    │  │      
+    │  ├─news
+    │  │      Detail.css
+    │  │      Detail.js
+    │  │      News.css
+    │  │      News.js
+    │  │      
+    │  ├─sandbox
+    │  │  │  NewsSandBox.css
+    │  │  │  NewsSandBox.js
+    │  │  │  
+    │  │  ├─audit-manage
+    │  │  │      Audit.js
+    │  │  │      AuditList.js
+    │  │  │      
+    │  │  ├─chat
+    │  │  │      chat.css
+    │  │  │      Chat.js
+    │  │  │      
+    │  │  ├─home
+    │  │  │      home.css
+    │  │  │      Home.js
+    │  │  │      
+    │  │  ├─news-manage
+    │  │  │      NewsAdd.css
+    │  │  │      NewsAdd.js
+    │  │  │      NewsCategory.js
+    │  │  │      NewsDraft.js
+    │  │  │      NewsPreview.css
+    │  │  │      NewsPreview.js
+    │  │  │      NewsUpdate.js
+    │  │  │      
+    │  │  ├─nopermission
+    │  │  │      NoPermission.css
+    │  │  │      NoPermission.js
+    │  │  │      
+    │  │  ├─publish-manage
+    │  │  │      Publish.js
+    │  │  │      Sunset.js
+    │  │  │      Unpublish.js
+    │  │  │      
+    │  │  ├─right-manage
+    │  │  │      RightList.js
+    │  │  │      RoleList.js
+    │  │  │      
+    │  │  └─use-manage
+    │  │          UserList.js
+    │  │          
+    │  └─__test__
+    │      │  Audit.test.js
+    │      │  AuditList.test.js
+    │      │  Detail.test.js
+    │      │  Home.test.js
+    │      │  LoginForm.test.js
+    │      │  News.test.js
+    │      │  NewsAdd.test.js
+    │      │  NoPermission.test.js
+    │      │  UserList.test.js
+    │      │  
+    │      └─__snapshots__
+    │              NoPermission.test.js.snap
+    │              
+    └─__mocks__
+            echarts.js
+            styleMock.js
+            
 
 ```
 
@@ -120,26 +178,32 @@
 
 
 ```
-│  index.js
-│  nodemon.json
-│  package.json
-│  
-├─middlewares
-│      auth.js
-│      cache.js
 │      
-├─mysql
-│      index.js
-│      
-│      
-└─routes
-        categoriesRouter
-        config.js
-        newsRouter.js
-        rightlistchildrenRouter.js
-        rightlistRouter.js
-        rolelistRouter.js
-        userRouter.js
+├─server
+│  │  index.js
+│  │  package-lock.json
+│  │  package.json
+│  │  
+│  ├─middlewares
+│  │      auth.js
+│  │      cache.js
+│  │      
+│  ├─mysql
+│  │      index.js
+│  │      
+│  ├─public
+│  │  └─upload
+│  │          
+│  └─routes
+│          categoriesRouter.js
+│          config.js
+│          files.js
+│          messageRouter.js
+│          newsRouter.js
+│          rightlistchildrenRouter.js
+│          rightlistRouter.js
+│          rolelistRouter.js
+│          userRouter.js
 ```
 
 
--- src/views/sandbox/chat/Chat.js
+++ src/views/sandbox/chat/Chat.js
@@ -221,3 +221,336 @@ const mapDispatchToProps = {
   }
 };
 export default connect(mapStateToProps, mapDispatchToProps)(Chat);
+
+```
+
+│  
+│  babel.config.js
+│  jest.config.js
+│  package-lock.json
+│  package.json
+│  README.md
+│  
+├─config
+│  │  webpack.config.js
+│  │  
+│  └─jest
+│          cssTransform.js
+│          fileTransform.js
+│          
+├─public
+│      favicon.ico
+│      index.html
+│      logo192.png
+│      logo512.png
+│      manifest.json
+│      mockServiceWorker.js
+│      robots.txt
+│      
+├─server
+│          
+└─src
+    │  .babelrc
+    │  App.css
+    │  App.js
+    │  babel.config.js
+    │  index.css
+    │  index.js
+    │  logo.svg
+    │  setupTests.js
+    │  
+    ├─components
+    │  ├─news-manage
+    │  │      NewsEditor.js
+    │  │      
+    │  ├─publish-manage
+    │  │      NewsPublish.js
+    │  │      usePublish.js
+    │  │      
+    │  ├─sandbox
+    │  │      index.css
+    │  │      SideMenu.js
+    │  │      TopHeader.js
+    │  │      withRoute.js
+    │  │      
+    │  ├─user-manage
+    │  │      UserForm.js
+    │  │      
+    │  └─__test__
+    │          NewsEditor.test.js
+    │          NewsPublish.test.js
+    │          SideMenu.test.js
+    │          TopHeader.test.js
+    │          
+    ├─image
+    │      JiqGstEfoWAOHiTxclqi.png
+    │      QasfAllzWOlzRzlqooai.jpg
+    │      
+    ├─mocks
+    │      handlers.js
+    │      server.js
+    │      
+    ├─redux
+    │  │  store.js
+    │  │  
+    │  └─reducers
+    │          CategoriesReducer.js
+    │          ChatMessageReducer.js
+    │          CollapsedReducer.js
+    │          CurrentUserReducer.js
+    │          NoticeListReducer.js
+    │          RightListReducer.js
+    │          RoleListReducer.js
+    │          
+    ├─router
+    │  │  DetailRoute.js
+    │  │  IndexRouter.js
+    │  │  LoginRoute.js
+    │  │  NewsRoute.js
+    │  │  Redirect.js
+    │  │  
+    │  └─__test__
+    │          IndexRouter.test.js
+    │          
+    ├─test
+    │      mockStore.js
+    │      
+    ├─utils
+    │      data.js
+    │      echarts.js
+    │      myAxios.js
+    │      
+    ├─views
+    │  ├─login
+    │  │      login.css
+    │  │      Login.js
+    │  │      LoginForm.js
+    │  │      
+    │  ├─news
+    │  │      Detail.css
+    │  │      Detail.js
+    │  │      News.css
+    │  │      News.js
+    │  │      
+    │  ├─sandbox
+    │  │  │  NewsSandBox.css
+    │  │  │  NewsSandBox.js
+    │  │  │  
+    │  │  ├─audit-manage
+    │  │  │      Audit.js
+    │  │  │      AuditList.js
+    │  │  │      
+    │  │  ├─chat
+    │  │  │      chat.css
+    │  │  │      Chat.js
+    │  │  │      
+    │  │  ├─home
+    │  │  │      home.css
+    │  │  │      Home.js
+    │  │  │      
+    │  │  ├─news-manage
+    │  │  │      NewsAdd.css
+    │  │  │      NewsAdd.js
+    │  │  │      NewsCategory.js
+    │  │  │      NewsDraft.js
+    │  │  │      NewsPreview.css
+    │  │  │      NewsPreview.js
+    │  │  │      NewsUpdate.js
+    │  │  │      
+    │  │  ├─nopermission
+    │  │  │      NoPermission.css
+    │  │  │      NoPermission.js
+    │  │  │      
+    │  │  ├─publish-manage
+    │  │  │      Publish.js
+    │  │  │      Sunset.js
+    │  │  │      Unpublish.js
+    │  │  │      
+    │  │  ├─right-manage
+    │  │  │      RightList.js
+    │  │  │      RoleList.js
+    │  │  │      
+    │  │  └─use-manage
+    │  │          UserList.js
+    │  │          
+    │  └─__test__
+    │      │  Audit.test.js
+    │      │  AuditList.test.js
+    │      │  Detail.test.js
+    │      │  Home.test.js
+    │      │  LoginForm.test.js
+    │      │  News.test.js
+    │      │  NewsAdd.test.js
+    │      │  NoPermission.test.js
+    │      │  UserList.test.js
+    │      │  
+    │      └─__snapshots__
+    │              NoPermission.test.js.snap
+    │              
+    └─__mocks__
+            echarts.js
+            styleMock.js
+            
+```
+
+
+客户端目录
+
+```
+
+│  babel.config.js
+│  package.json
+│  
+├─config
+│      webpack.config.js
+│      
+└─src
+    │  App.js
+    │  index.css
+    │  index.js
+    │  logo.svg
+    │  
+    ├─components
+    │  ├─news-manage
+    │  │      NewsEditor.js
+    │  │      
+    │  ├─publish-manage
+    │  │      NewsPublish.js
+    │  │      usePublish.js
+    │  │      
+    │  ├─sandbox
+    │  │      index.css
+    │  │      SideMenu.js
+    │  │      TopHeader.js
+    │  │      withRoute.js
+    │  │      
+    │  ├─user-manage
+    │  │      UserForm.js
+    │  │      
+    │  └─__test__
+    │          NewsEditor.test.js
+    │          NewsPublish.test.js
+    │          SideMenu.test.js
+    │          TopHeader.test.js
+    │          
+    ├─image
+    │      JiqGstEfoWAOHiTxclqi.png
+    │      QasfAllzWOlzRzlqooai.jpg
+    │      
+    ├─mocks
+    │      handlers.js
+    │      server.js
+    │            
+    ├─redux
+    │  │  store.js
+    │  │  
+    │  └─reducers
+    │          CategoriesReducer.js
+    │          ChatMessageReducer.js
+    │          CollapsedReducer.js
+    │          CurrentUserReducer.js
+    │          NoticeListReducer.js
+    │          RightListReducer.js
+    │          RoleListReducer.js
+    │          
+    │          
+    ├─router
+    │  │  DetailRoute.js
+    │  │  IndexRouter.js
+    │  │  LoginRoute.js
+    │  │  NewsRoute.js
+    │  │  Redirect.js
+    │  │  
+    │  └─__test__
+    │          IndexRouter.test.js
+    │
+    ├─test
+    │      mockStore.js
+    │     
+    ├─utils
+    │      data.js
+    │      echarts.js 
+    │      myAxios.js
+    │      
+    └─views
+        ├─login
+        │      login.css
+        │      Login.js
+        │      LoginForm.js
+        │      
+        ├─news
+        │      Detail.css
+        │      Detail.js
+        │      News.css
+        │      News.js
+        │      
+        └─sandbox
+            │  NewsSandBox.css
+            │  NewsSandBox.js
+            │  
+            ├─audit-manage
+            │      Audit.js
+            │      AuditList.js
+            │      
+            ├─chat
+            │      chat.css
+            │      Chat.js
+            │            │      
+            ├─home
+            │      home.css
+            │      Home.js
+            │      
+            ├─news-manage
+            │      NewsAdd.css
+            │      NewsAdd.js
+            │      NewsCategory.js
+            │      NewsDraft.js
+            │      NewsPreview.css
+            │      NewsPreview.js
+            │      NewsUpdate.js
+            │      
+            ├─nopermission
+            │      Nopermission.css
+            │      NoPermission.js
+            │      
+            ├─publish-manage
+            │      Publish.js
+            │      Sunset.js
+            │      Unpublish.js
+            │      
+            ├─right-manage
+            │      RightList.js
+            │      RoleList.js
+            │      
+            └─use-manage
+                    UserList.js
+
+```
+
+
+
+服务器server目录
+
+
+```
+│  index.js
+│  nodemon.json
+│  package.json
+│  
+├─middlewares
+│      auth.js
+│      cache.js
+│      
+├─mysql
+│      index.js
+│      
+│      
+└─routes
+        categoriesRouter
+        config.js
+        newsRouter.js
+        rightlistchildrenRouter.js
+        rightlistRouter.js
+        rolelistRouter.js
+        userRouter.js
+```
