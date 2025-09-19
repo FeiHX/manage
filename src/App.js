3469f707bff4b9ec46e7922d1481e081837d4616
FeiHX <93259045+FeiHX@users.noreply.github.com>
2025-09-19 13:26:51 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,14 +3,14 @@ import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
 import IndexRouter from "./router/IndexRouter.js";
 import { persistor, store } from "./redux/store.js";
-import { HashRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 export default function App() {
   return (
     <Provider store={store}>
       <PersistGate persistor={persistor}>
-        <HashRouter>
+        <BrowserRouter basename='/'>
           <IndexRouter />
-        </HashRouter>
+        </BrowserRouter>
       </PersistGate>
     </Provider>
   );
